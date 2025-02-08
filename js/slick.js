4131acfce04b2d9916d3697a7f0a15011e149330
olesyamaniuk <maniukolesya222@gmail.com>
2025-02-08 13:36:48 +0200

    1

---
+++ js/slick.js
@@ -0,0 +1,43 @@
+
+
+$(function () {
+  $('.gallery__list').slick({
+    infinite: true,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    variableWidth: true,
+    autoplay: true,
+    autoplaySpeed: 3500,
+    speed: 1000,
+    dots: false,
+    centerMode: true,
+    cssEase: 'linear',
+    prevArrow: '.gallery-list-left',
+    nextArrow: '.gallery-list-right',
+  });
+});
+
+
+$(function () {
+  $('.reviews-list').slick({
+    infinite: true,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    variableWidth: true,
+    autoplay: true,
+    autoplaySpeed: 3500,
+    speed: 1000,
+    dots: true,
+    centerMode: true,
+    cssEase: 'linear',
+    prevArrow: '.reviews-list-left',
+    nextArrow: '.reviews-list-right',
+  });
+});
+
+
+
+
+
+
+
